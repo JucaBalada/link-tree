0a8d5973af641307c60da7a39a271feff23008b4
JucaBalada <gomesdossantospablo777@gmail.com>
2023-08-28 10:08:42 -0300

    :tada: initial commit

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 const abas = document.querySelectorAll(".aba");
 
 abas.forEach(aba => {
-    // passo 2 - dar um jeito de identificar o clique no elemento da aba
+    // dar um jeito de identificar o clique no elemento da aba
     aba.addEventListener("click", function() {
 
         if(aba.classList.contains("selecionado")){
@@ -18,22 +18,39 @@ abas.forEach(aba => {
 });
 
 function selecionarAba(aba) {
-    // passo 3 - quando o usuário clicar, desmarcar a aba selecionada
+    // quando o usuário clicar, desmarcar a aba selecionada
     const abaSelecionada = document.querySelector(".aba.selecionado");
     abaSelecionada.classList.remove("selecionado");
 
-    // passo 4 - marcar a aba clicada como selecionado
+    //marcar a aba clicada como selecionado
     aba.classList.add("selecionado");
 }
 
 function mostrarInformacoesDaAba(aba){
-    // passo 5 - esconder o conteúdo anterior
+    // esconder o conteúdo anterior
     const informacaoSelecionada = document.querySelector(".informacao.selecionado");
     informacaoSelecionada.classList.remove("selecionado");
 
-    // passo 6 - mostrar o conteúdo da aba selecionada
+    // mostrar o conteúdo da aba selecionada
     const idDoElementoDeInformacoesDaAba = `informacao-${aba.id}`
 
     const informacaoASerMostrada = document.getElementById(idDoElementoDeInformacoesDaAba)
     informacaoASerMostrada.classList.add("selecionado")
 }
+
+
+function toggleMode(){
+    const html = document.documentElement
+    html.classList.toggle('light')
+
+    const img = document.querySelector(".minhas-informacoes img")
+    
+    if(html.classList.contains('light')){
+        img.setAttribute('src', 'img/oculos-light.svg')
+    }else{
+        img.setAttribute('src', 'img/programming-animate.svg')
+    }
+
+    
+}
+
